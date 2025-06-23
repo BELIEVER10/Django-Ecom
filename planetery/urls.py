6c7de7942bc6c7e824b472a08085bfb270701edb
sujan <sujandotel181@gmail.com>
2025-06-23 22:23:29 +0545

    some thing added necessary for static file serve

--- planetery/urls.py
+++ planetery/urls.py
@@ -37,6 +37,12 @@ urlpatterns = [
     
     
     
-    
 
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+
+#  + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_URL)
